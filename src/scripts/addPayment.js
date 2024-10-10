00b4331beda65cdc8e946c52305d5e2b74c63960
Agustín Concollato <agustinconcollato@gmail.com>
2024-10-10 01:56:00 -0300

    agregar pagos de los meses a elección

--- src/scripts/addPayment.js
+++ src/scripts/addPayment.js
@@ -1,10 +1,20 @@
 const addPayment = ({ id, e }) => {
+    const selectedMonth = document.getElementById('months-payment').value
     const formData = new FormData(e.target)
+    const selectedDate = new Date()
+    const currentdate = new Date()
 
-    const formObject = {
-        id,
-        data: new Date().getTime()
+    let data = currentdate.getTime()
+
+    if (currentdate.getMonth() != selectedMonth) {
+        selectedDate.setMonth(parseInt(selectedMonth))
+        selectedDate.setDate(1)
+        selectedDate.setHours(0, 0, 0, 0)
+
+        data = selectedDate
     }
+
+    const formObject = { id, data }
 
     formData.forEach((value, key) => {
         formObject[key] = value;
